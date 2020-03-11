87f61fe367aaa8f5cbbd4e26a3285a0f830cd696
Andrew Gozzard <gozzard@gmail.com>
2020-03-11 10:52:17 +0800

    Better Show State instance

--- src/EduRISC/State.hs
+++ src/EduRISC/State.hs
@@ -4,6 +4,7 @@ import Data.Int
 import Data.Map.Lazy (Map)
 import Data.Map.Lazy as Map
 import Data.Word
+import Numeric (showHex)
 
 import EduRISC.Registers
 
@@ -12,16 +13,19 @@ type MemMap = Map Word16 Word16
 data CpuState = CpuState { state_regs :: RegMap, state_mem :: MemMap }
 
 instance Show CpuState where
-  show = show . state_regs
+  show = (Map.foldlWithKey f "") . state_regs
+    where
+      f s r v = s ++ showReg r v
+      showReg r v = show r ++ ":" ++ showHex v "\t"
 
 initState :: Word16 -> [Word16] -> CpuState
 initState bootAddress memImage = CpuState { state_regs = initReg bootAddress, state_mem = initMem memImage}
 
+nullReg :: RegMap
+nullReg = Map.fromAscList $ zip (enumFrom R0) (repeat 0)
+
 initReg :: Word16 -> RegMap
-initReg = Map.singleton RF
+initReg v = Map.insert RF v nullReg
 
 initMem :: [Word16] -> MemMap
 initMem = Map.fromAscList . (zip [0..])
-
-dumpCpuState :: CpuState -> String
-dumpCpuState s = show $ state_regs s
